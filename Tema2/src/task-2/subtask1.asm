e8d42d03fd0fe38f8c3548af0bc71d98e300543e
Stoica Liviu <liviu.stoica0709@stud.acs.upb.ro>
2024-04-16 22:39:29 +0300

    Started task2

--- Tema2/src/task-2/subtask1.asm
+++ Tema2/src/task-2/subtask1.asm
@@ -2,6 +2,26 @@
 
 ; declare your structs here
 
+
+
+section .data
+    format_admin db "Admin: %d", 10, 0
+    format_prio db "Priority: %d", 10, 0
+    format_user db "Username: %s", 10, 0
+    format_passkey db "Passkey: %hu", 10, 0
+    format_int db "Edx: %d", 10, 0
+    format_int_ecx db "Ecx: %d", 10, 0
+    struc creds
+        .passkey:    resw 1
+        .username:   resb 51
+    endstruc
+
+    struc request
+        .admin: resb 1
+        .prio: resb 1
+        .login: resb creds_size
+    endstruc
+
 section .text
     global sort_requests
     extern printf
@@ -16,7 +36,74 @@ sort_requests:
     ;; DO NOT MODIFY
 
     ;; Your code starts here
+    mov edx, 0
+    push edx
+    push ecx
+    push edx
+    push format_int
+    call printf
+    pop eax
+    pop eax
+    pop ecx
+    pop edx
+for:
+    ; check if admin
+    xor eax, eax
+    imul edi, edx, 55 ; 55 = sizeof(request)
+    mov al, [ebx + edi] ; al is 1 byte = sizeof(char) = sizeof(request.admin)
+    push edx
+    push ecx
+    push eax
+    push format_admin
+    call printf
+    pop eax
+    pop eax
+    pop ecx
+    pop edx
 
+    ; get priority
+    xor eax, eax
+    mov al, [ebx + 1 + edi]
+    push edx
+    push ecx
+    push eax
+    push format_prio
+    call printf
+    pop eax
+    pop eax
+    pop ecx
+    pop edx
+
+    ; get passkey
+    mov eax, [ebx + 2 + edi] ; eax = &request.login.passkey
+    push edx
+    push ecx
+    push eax
+    push format_passkey
+    call printf
+    pop eax
+    pop eax
+    pop ecx
+    pop edx
+    ; get username
+    ; lea not mov bcz we need to copy address of username
+    lea eax, [ebx + 4 + edi] ; eax = &request.login.username
+    push edx
+    push ecx
+    push eax
+    push format_user
+    call printf
+    pop eax
+    pop eax
+    pop ecx
+    pop edx
+
+
+    
+
+    inc edx
+    cmp edx, ecx
+    jng for
     ;; Your code ends here
 
     ;; DO NOT MODIFY
