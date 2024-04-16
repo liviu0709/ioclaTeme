102b3ba6aefd5a1a111b153bbd59ae18d1adbc4c
Stoica Liviu <liviu.stoica0709@stud.acs.upb.ro>
2024-04-16 19:46:44 +0300

    Task 1

--- Tema2/src/task-1/check_permission.asm
+++ Tema2/src/task-1/check_permission.asm
@@ -5,6 +5,9 @@ extern ant_permissions
 extern printf
 global check_permission
 
+section .data
+    format_int db "Int: %u", 10, 0
+
 section .text
 
 check_permission:
@@ -16,15 +19,38 @@ check_permission:
     mov     eax, [ebp + 8]  ; id and permission
     mov     ebx, [ebp + 12] ; address to return the result
     ;; DO NOT MODIFY
-   
-    ;; Your code starts here
 
+    ;; Your code starts here
+    xor eax, eax ; clear
+    mov al, [ebp + 8 + 3] ; copy most significant 8 bits to get id
+    mov ecx, [ant_permissions + eax * 4] ;; * 4 because unsigned int is 4 bytes
+
+    mov eax, [ebp + 8] ; copy permission + id
+    shl eax, 8 ; shift 8 bits to the left to get permission
+    shl ecx, 8 ; to compare registers bit with bit
+    ; eax has requested rooms
+    ; ebx has allowed rooms
+    ; if eax has bits that are not in ebx, then it is not allowed
+    mov edx, eax ; copy eax for later use
+    and eax, ecx ; if eax has bits that are not in ebx, then it is not allowed
+    cmp eax, edx
+    jne not_allowed
+    ; if eax is equal to ecx, then it is allowed
+    mov eax, 1 ; return 1
+
+    jmp done ; skip not_allowed
+
+not_allowed:
+    mov eax, 0 ; return 0
+
+done:
+    mov [ebx], eax ; save solution
     ;; Your code ends here
-    
+
     ;; DO NOT MODIFY
 
     popa
     leave
     ret
-    
+
     ;; DO NOT MODIFY
