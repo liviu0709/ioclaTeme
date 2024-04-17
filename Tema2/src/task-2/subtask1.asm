287bebb56a09ba090b472f2c0b9f926fbe4cd755
Stoica Liviu <liviu.stoica0709@stud.acs.upb.ro>
2024-04-17 21:25:28 +0300

    Almost finished task2.1

--- Tema2/src/task-2/subtask1.asm
+++ Tema2/src/task-2/subtask1.asm
@@ -46,7 +46,97 @@ sort_requests:
     pop eax
     pop ecx
     pop edx
+    mov esi, ecx
+
+
+outer_loop:
+    dec esi
+    jz done ; cond oprire
+    mov edi, ebx ; element curent
+    mov edx, esi ; counter loop inner
+
+inner_loop:
+    xor eax, eax
+    ; compar adminii
+    mov al, [edi]
+    cmp al, [edi + request_size]
+    jl swap ; daca ordinea nu e buna, fac swap
+
+    cmp al, [edi + request_size] ; daca unul e admin si altul nu, trec mai departe
+    jne next
+
+    ; compar prioritatile daca adminii sunt egali
+    mov al, [edi + request.prio]
+    cmp al, [edi + request.prio + request_size]
+    jg swap ; daca prioritatea e mai mare, fac swap
+
+    cmp al, [edi + request.prio + request_size] ; daca nu au prioritatea egala, trec mai departe
+    jne next
+
+    ; avand prioritate si grad de admin egal, compar user ;/
+    push ecx    ; salvam registrii ca sa nu dea segfault
+    push ebx
+    push esi
+    mov ecx, 51
+    lea esi, [edi + request.login + 2] ; adresa user curent
+    lea ebx, [edi + request.login + 2 + request_size] ; adresa user urmator
+compare_char:
+    xor eax, eax
+    mov al, [esi] ; caracter curent user curent
+    cmp al, [ebx] ; caracter curent user urmator
+    jne not_equal ; daca nu sunt egale, pot afla ordinea lor!
+    inc esi ; trec la urmatorul caracter
+    inc ebx ; trec la urmatorul caracter
+    dec ecx ; decrementez contorul
+    jnz compare_char ; daca nu am terminat, repet
+
+    pop esi ; da segfault daca nu salvam valorile
+    pop ebx
+    pop ecx
+
+    jmp next ; dam skip la swap
+
+not_equal:
+    pop esi ; da segfault daca nu salvam valorile
+    pop ebx
+    pop ecx
+    ; daca nu sunt ordonate alfabetic, fac swap
+    jg swap
+    ; altfel trec mai departe
+    jmp next
+
+
+
+swap:
+    ; salvez ebx
+    push ebx
+    mov ebx, request_size
+swap_loop:
+    mov al, [edi]
+    xchg al, [edi + request_size]
+    mov [edi], al
+    inc edi
+    dec ebx
+    jnz swap_loop
+    sub edi, request_size
+    
+    pop ebx
+
+next:
+    add edi, request_size
+    dec edx
+    jnz inner_loop
+    jmp outer_loop
+
+done:
+
+    mov edx, 0
+    ;mov ecx, 0
+
 for:
+
+
+
     ; check if admin
     xor eax, eax
     imul edi, edx, 55 ; 55 = sizeof(request)
@@ -99,11 +189,12 @@ for:
     pop edx
 
 
-    
+
 
     inc edx
     cmp edx, ecx
-    jng for
+    jne for
+
     ;; Your code ends here
 
     ;; DO NOT MODIFY
