326ad8a49d1bb23b897cfeda81899122cf28c240
Stoica Liviu <liviu.stoica0709@stud.acs.upb.ro>
2024-05-13 18:18:54 +0300

    Task 1

--- Tema3/src/task-1/paranthesinator.asm
+++ Tema3/src/task-1/paranthesinator.asm
@@ -23,13 +23,13 @@ for:
     mov dl, [ebx + ecx]
     cmp edx, 0
     je finish
-    push ecx
-    push edx
-    push format_chr
-    call printf
-    pop eax
-    pop eax
-    pop ecx
+    ; push ecx
+    ; push edx
+    ; push format_chr
+    ; call printf
+    ; pop eax
+    ; pop eax
+    ; pop ecx
 
     ; verific (
     xor edx, edx
@@ -51,17 +51,96 @@ skip2:
     inc ecx
     jmp for
 finish:
-    
+
+    cmp edi, 0
+    jne fail
+
+    mov edi, 0
+
+for2:
+    xor edx, edx
+    mov dl, [ebx + ecx]
+    cmp edx, 0
+    je finish2
+    ; push ecx
+    ; push edx
+    ; push format_chr
+    ; call printf
+    ; pop eax
+    ; pop eax
+    ; pop ecx
+
+    ; verific (
+    xor edx, edx
+    mov dl, [ebx + ecx]
+    cmp dl, 91
+    jne skip3
+    inc edi
+skip3:
+
+    ; verific )
+    cmp dl, 93
+    jne skip4
+    dec edi
+    cmp edi, -1
+    je fail
+skip4:
+
+
+    inc ecx
+    jmp for2
+
+finish2:
+
+    cmp edi, 0
+    jne fail
+    mov edi, 0
+for3:
+    xor edx, edx
+    mov dl, [ebx + ecx]
+    cmp edx, 0
+    je finish3
+    ; push ecx
+    ; push edx
+    ; push format_chr
+    ; call printf
+    ; pop eax
+    ; pop eax
+    ; pop ecx
+
+    ; verific (
+    xor edx, edx
+    mov dl, [ebx + ecx]
+    cmp dl, 123
+    jne skip5
+    inc edi
+skip5:
+
+    ; verific )
+    cmp dl, 125
+    jne skip6
+    dec edi
+    cmp edi, -1
+    je fail
+skip6:
+
+
+    inc ecx
+    jmp for3
+
+finish3:
+
     cmp edi, 0
     je succes
+
     ; sa-nceapa concursul
 fail:
     popa
-    mov eax, 0
+    mov eax, 1
     jmp skip_suc
 succes:
     popa
-    mov eax, 1
+    mov eax, 0
 skip_suc:
     leave
 
