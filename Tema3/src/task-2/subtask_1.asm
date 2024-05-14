ed450f0022d087062b6c03c1c1254e6f6924b95d
Stoica Liviu <liviu.stoica0709@stud.acs.upb.ro>
2024-05-14 17:35:43 +0300

    Subtask 1 - comments

--- Tema3/src/task-2/subtask_1.asm
+++ Tema3/src/task-2/subtask_1.asm
@@ -1,13 +1,8 @@
 ; subtask 1 - qsort
-section .data
-    format_int db "Start: %u Stop: %u", 10, 0
-    format_int1 db "Apel1: %u Stop: %u", 10, 0
-    format_int2 db "Final?: %u Stop: %u", 10, 0
 
 section .text
     global quick_sort
     ;; no extern functions allowed
-extern printf
 quick_sort:
     ;; create the new stack frame
     enter 0, 0
@@ -19,35 +14,28 @@ quick_sort:
     mov ebx, [ebp + 12] ; start
     mov ecx, [ebp + 16] ; end
 
-    cmp ebx, ecx
-
+    cmp ebx, ecx ; condition to stop
+    ; (one element cant be sorted)
     jge stop
-    push ecx
-    push ebx
-    mov edx, ebx
 
-    pusha
-    push ecx
-    push ebx
-    push format_int
-    call printf
-    add esp, 12
-    popa
+    push ecx ; save end
+    push ebx ; save start
+    mov edx, ebx ; index
+
 for:
+    ; condition to swap
     mov edi, [eax + 4 * edx]
     cmp edi, [eax + 4 * ecx]
     jge no_swap
 
-
+    ; swapping arr[edx] with arr[ebx]
     push edi
     mov edi, [eax + 4 * ebx]
     mov [eax + 4 * edx], edi
     pop edi
     mov [eax + 4 * ebx], edi
     inc ebx
-    jmp skip
 no_swap:
-skip:
     inc edx
     cmp edx, ecx
     jne for
@@ -62,26 +50,15 @@ skip:
     mov [eax + 4 * ecx], edi
 
 ; split array
-    mov edx, ebx
-    pop ebx
-    pop ecx
+    mov edx, ebx ; pivot
+    pop ebx ; start first call
+    pop ecx ; stop last call
 ; check if array has more than 2 elems otherwise we finish
     dec edx ; dont include pivot
 
     cmp edx, ecx ; if we called the func with the same data we send to recursivity
     ; just stop
     je stop
-
-
-
-
-    pusha
-    push edx
-    push ebx
-    push format_int
-    call printf
-    add esp, 12
-    popa
 
     push edx ; stop
     push ebx ; start
@@ -92,17 +69,9 @@ skip:
     inc edx
     inc edx ; dont include pivot
 
-    pusha
-    push ecx
-    push edx
-    push format_int
-    call printf
-    add esp, 12
-    popa
-
     push ecx ; stop
     push edx ; start
-    push eax
+    push eax ; array
     call quick_sort
     add esp, 12
 
